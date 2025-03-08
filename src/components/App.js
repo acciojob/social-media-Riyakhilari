1f38615a61ef4b1bc09172ec785ccd31fdecaa65
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-03-08 20:14:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
-
 import React from "react";
-import './../styles/App.css';
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+import PostsList from "./components/PostsList";
+import UsersList from "./components/UsersList";
+import NotificationsList from "./components/NotificationsList";
+import AddPostForm from "./components/AddPostForm";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Router>
+      <div className="app">
+        <header className="header">
+          <h1>GenZ</h1>
+          <nav>
+            <NavLink to="/" className="nav-link">Posts</NavLink>
+            <NavLink to="/users" className="nav-link">Users</NavLink>
+            <NavLink to="/notifications" className="nav-link">Notifications</NavLink>
+          </nav>
+          <button className="refresh-btn">Refresh Notifications</button>
+        </header>
 
-export default App
+        <main>
+          <Routes>
+            <Route path="/" element={<><AddPostForm /><PostsList /></>} />
+            <Route path="/users" element={<UsersList />} />
+            <Route path="/notifications" element={<NotificationsList />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
